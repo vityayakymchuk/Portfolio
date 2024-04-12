1a99f41fe4a595bd475275774d6f76cc85b37cb2
DimaKarvackij <dimakarvackij777@gmail.com>
2024-04-12 17:29:24 +0300

    commit update

--- src/js/covers.js
+++ src/js/covers.js
@@ -3,16 +3,16 @@ function coverPage() {
     const result  = 
 }
 
-// Вибираємо елемент секції "Covers"
+
 const coversSection = document.querySelector('#covers-section');
 
-// Створюємо новий Intersection Observer
+
 const observer = new IntersectionObserver((entries, observer) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      // Код для запуску анімації коли секція "Covers" у зоні видимості
+      
     } else {
-      // Код для зупинки анімації коли секція "Covers" поза зоною видимості
+     
     }
   });
 });
