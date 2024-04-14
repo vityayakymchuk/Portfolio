506c9c6803de80aaa443451c4dc3cba1927895b3
DimaKarvackij <dimakarvackij777@gmail.com>
2024-04-14 08:02:16 +0300

    commit

--- src/js/covers.js
+++ src/js/covers.js
@@ -1,28 +1,17 @@
-//Функція, що перевіряє, чи є елемент в області видимості
-// function isInViewport(element) {
-//   const rect = element.getBoundingClientRect();
-//   return (
-//     rect.top >= 0 &&
-//     rect.left >= 0 &&
-//     rect.bottom <=
-//       (window.innerHeight || document.documentElement.clientHeight) &&
-//     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-//   );
-// }
-
-// // Функція, що встановлює анімацію для секції "Covers"
-// function setAnimation() {
-//   const coversSection = document.querySelector('.js_enable_animation');
-//   if (isInViewport(coversSection)) {
-//     // Включення анімації
-//     coversSection.classList.add('cover');
-//   } else {
-//     // Вимкнення анімації
-//     coversSection.classList.remove('cover');
-//   }
-// }
-
-// // Виклик функції при завантаженні сторінки та при прокрутці
-// const page = document.addEventListener('DOMContentLoaded', setAnimation);
-// const pageDocument = window.addEventListener('scroll', setAnimation);
-// export { isInViewport, setAnimation, page, pageDocument };
+export function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  const windowHeight =
+    window.innerHeight || document.documentElement.clientHeight;
+  const windowWidth = window.innerWidth || document.documentElement.clientWidth;
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= windowHeight &&
+    rect.right <= windowWidth
+  );
+}
+export function setAnimation() {
+  const coversSection = document.querySelector('.js_enable_animation');
+  coversSection.classList.toggle('marquee__line', isInViewport(coversSection));
+}
+setAnimation();
--- src/js/main.js
+++ src/js/main.js
@@ -17,3 +17,5 @@ import {
   menuClosedTablAndDeskt,
 } from './header-modal';
 >>>>>>> 360beaff34f3ef511dd81e56ce7f2f4f1486bc17
+
+import{isInViewport, setAnimation} from "./covers";
