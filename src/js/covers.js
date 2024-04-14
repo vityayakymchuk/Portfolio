ccc134683af09cebcca06a0a32e1b65b7cfc44ee
DimaKarvackij <dimakarvackij777@gmail.com>
2024-04-14 09:35:02 +0300

    commit

--- src/js/covers.js
+++ src/js/covers.js
@@ -1,17 +1,17 @@
-export function isInViewport(element) {
-  const rect = element.getBoundingClientRect();
-  const windowHeight =
-    window.innerHeight || document.documentElement.clientHeight;
-  const windowWidth = window.innerWidth || document.documentElement.clientWidth;
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= windowHeight &&
-    rect.right <= windowWidth
-  );
-}
-export function setAnimation() {
-  const coversSection = document.querySelector('.js_enable_animation');
-  coversSection.classList.toggle('marquee__line', isInViewport(coversSection));
-}
-setAnimation();
+// export function isInViewport(element) {
+//   const rect = element.getBoundingClientRect();
+//   const windowHeight =
+//     window.innerHeight || document.documentElement.clientHeight;
+//   const windowWidth = window.innerWidth || document.documentElement.clientWidth;
+//   return (
+//     rect.top >= 0 &&
+//     rect.left >= 0 &&
+//     rect.bottom <= windowHeight &&
+//     rect.right <= windowWidth
+//   );
+// }
+// export function setAnimation() {
+//   const coversSection = document.querySelector('.js_enable_animation');
+//   coversSection.classList.toggle('marquee__line', isInViewport(coversSection));
+// }
+// setAnimation();
