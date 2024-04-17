77d5bc016bbd8b6c91d3060a8790d183d898c259
Ihor4yk <107033477+Ihor4yk@users.noreply.github.com>
2024-04-17 12:44:32 +0300

    accordion code fix!

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,18 +1,28 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
+document.addEventListener("DOMContentLoaded", function () {
+  const firstAccordionItem = document.querySelector('.accordion-item');
+  const firstAccordionContent = firstAccordionItem.querySelector('.is-hidden');
+
+  firstAccordionItem.classList.add('is-active');
+  firstAccordionContent.style.maxHeight = firstAccordionContent.scrollHeight + 'px';
+});
+
 const accordionBtns = document.querySelectorAll('.accordion-button-up-down');
-const firstAccordionItem = document.querySelector('.accordion-item');
-const firstAccordionContent = firstAccordionItem.querySelector('.is-hidden');
 
-firstAccordionItem.classList.add('is-active');
-firstAccordionContent.style.maxHeight = firstAccordionContent.scrollHeight + 'px';
-firstAccordionItem.querySelector('.accordion-button-up-down use').setAttribute('href', './img/sprite.svg#icon-accordion-up');
-
-accordionBtns.forEach((btn) => {
+accordionBtns.forEach((btn, index) => {
   btn.addEventListener('click', () => {
     const accordionItem = btn.closest('.accordion-item');
     const accordionContent = accordionItem.querySelector('.is-hidden');
+
+    const activeAccordionItem = document.querySelector('.accordion-item.is-active');
+    if (activeAccordionItem && activeAccordionItem !== accordionItem) {
+      const activeAccordionContent = activeAccordionItem.querySelector('.is-hidden');
+      activeAccordionItem.classList.remove('is-active');
+      activeAccordionContent.style.maxHeight = 0;
+      activeAccordionItem.querySelector('.accordion-button-up-down use').setAttribute('href', './img/sprite.svg#icon-accordian-down');
+    }
 
     if (accordionItem.classList.contains('is-active')) {
       accordionItem.classList.remove('is-active');
@@ -25,4 +35,3 @@ accordionBtns.forEach((btn) => {
     }
   });
 });
-
